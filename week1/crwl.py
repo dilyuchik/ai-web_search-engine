e1a74f363af28167ba2d0b26c883f36822864e44
dilyuchik <daria.ilyuchik@gmx.de>
2024-12-12 12:35:52 +0100

    Updated based on content of last lecture

--- week1/crwl.py
+++ week1/crwl.py
@@ -10,10 +10,10 @@ agenda = [start_url]
 while agenda:
     url = agenda.pop()
     print("Get ",url)
-    r = requests.get(url)
+    r = requests.get(url) # get content of a URL
     print(r, r.encoding)
     if r.status_code == 200:
         print(r.headers)
-        soup = BeautifulSoup(r.content, 'html.parser')
+        soup = BeautifulSoup(r.content, 'html.parser') # parse html content, giving all headers or anchors e.g.
         print(soup.find_all('a'))
         
--- week2/myapp2.py
+++ week2/myapp2.py
@@ -8,6 +8,7 @@ def start():
 
 @app.route("/reversed")
 def reversed():
-    rev = request.args['rev'][::-1]
-    return render_template('reversed.html', rev=rev)
+    rev = request.args['rev'][::-1] # process argument, such that it is displayed in reversed order
+                                    # args is a dictionary organized by names given to form elements
+    return render_template('reversed.html', rev=rev) # can also give list of search hits e.g., and loop through them
 
