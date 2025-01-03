679cefe3acff5374a6208adb7a21102e2a85fe74
dilyuchik <daria.ilyuchik@gmx.de>
2025-01-03 17:33:41 +0100

    removed not needed comments

--- submission/search_engine.py
+++ submission/search_engine.py
@@ -28,11 +28,7 @@ def search():
     print(f"\nPages containing your search query \'{query}\':")
     for url in search_results:
        print(f"- {url}")
-    """
-    rev = request.args['rev'][::-1] # process argument, such that it is displayed in reversed order
-                                    # args is a dictionary organized by names given to form elements
-    return render_template('reversed.html', rev=rev) # can also give list of search hits e.g., and loop through them
-    """
+
     return render_template('search.html', search_results=search_results)
 
 if __name__ == "__main__":
